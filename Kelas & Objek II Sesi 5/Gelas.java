623a2d778bb11154deb0118923be3fb2f2268c52
rendypratamaputra <126095967+rendypratamaputra@users.noreply.github.com>
2024-03-12 16:04:00 +0700

    Add files via upload

---
+++ Kelas & Objek II Sesi 5/Gelas.java
@@ -0,0 +1,22 @@
+	package KelasObjekSesi4;
+	
+	public class Gelas {
+		// Atribut untuk menyimpan warna gelas
+		    private String warna;
+		 
+		    // Konstruktor untuk menginisialisasi warna gelas
+		    public Gelas(String w) {
+		        warna = w;
+		    }
+	
+		 // Setter untuk mengubah warna gelas
+		    void setWarna(String w) {
+		        warna = w;
+		    }
+	
+		 // Getter untuk mendapatkan warna gelas
+		    String getWarna() {
+		        return warna;
+		    }
+		}
+	
